e78eeddee04a1e8e1de3b06755366cabe2172f8e
malin01 <malin01@fenbi.com>
2020-04-01 23:15:26 +0800

    ADD: blog

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost/site', { useNewUrlParser: true }),
+        BlogModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/blog/blog.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './interfaces/post.interface';
+import { CreatePostDTO } from './dto/create-post.dto';
+
+@Injectable()
+export class BlogService {
+
+    constructor(@InjectModel('Post') private readonly postModel: Model<Post>) { }
+
+    // get all
+    async getPosts(): Promise<Post[]> {
+        const posts = await this.postModel.find().exec();
+        return posts;
+    }
+
+    // get single
+    async getPost(postID): Promise<Post> {
+        const post = await this.postModel
+            .findById(postID)
+            .exec();
+        return post;
+    }
+
+    // c
+    async addPost(createPostDTO: CreatePostDTO): Promise<Post> {
+        const newPost = await this.postModel(createPostDTO);
+        return newPost.save();
+    }
+
+    // u
+    async editPost(postID, createPostDTO: CreatePostDTO): Promise<Post> {
+        const editedPost = await this.postModel
+            .findByIdAndUpdate(postID, createPostDTO, { new: true });
+        return editedPost;
+    }
+
+    //d
+    async deletePost(postID): Promise<any> {
+        const deletedPost = await this.postModel
+            .findByIdAndRemove(postID);
+        return deletedPost;
+    }
+
+}
