504f3f6eac1601e363855d5b1f00b293b05ee7b6
malin01 <malin01@fenbi.com>
2020-04-04 11:22:14 +0800

    ADD: middleware

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogSchema } from './schemas/blog.schema';
+import { AuthMiddleware } from './auth.middleware';
 
 @Module({
     imports: [
@@ -11,4 +12,8 @@ import { BlogSchema } from './schemas/blog.schema';
     controllers: [BlogController],
     providers: [BlogService]
 })
-export class BlogModule { }
+export class BlogModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AuthMiddleware).forRoutes(BlogController);
+    }
+}
